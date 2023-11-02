b15e527a92b1364045af727313656eff1746b48d
StefaneAmorim <104101680+StefaneAmorim@users.noreply.github.com>
2023-11-02 18:36:20 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+require '../database/connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
+    $stmt->execute(['username' => $username]); 
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        session_start();
+        $_SESSION['user_id'] = $user['id'];
+        header("Location: user_page.php");
+    } else {
+        // login failed
+    }
+}
